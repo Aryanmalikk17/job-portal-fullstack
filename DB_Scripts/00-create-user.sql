ca1af67cd291b21c533f0944f0c9b87c4874133b
aadityamalikk <adityajaihind7@gmail.com.com>
2025-12-08 16:47:50 +0530

    🔧 CRITICAL FIX: Update MySQL user initialization script to use correct production password instead of old password

--- DB_Scripts/00-create-user.sql
+++ DB_Scripts/00-create-user.sql
@@ -3,13 +3,13 @@ DROP USER IF EXISTS 'jobportal'@'%';
 DROP USER IF EXISTS 'jobportal'@'localhost';
 
 -- Create user that can connect from any host (Docker containers)
-CREATE USER 'jobportal'@'%' IDENTIFIED BY 'jobportal123';
+CREATE USER 'jobportal'@'%' IDENTIFIED BY 'ZpluseAppPass2025!';
 
 -- Grant all privileges to the user
 GRANT ALL PRIVILEGES ON jobportal.* TO 'jobportal'@'%';
 
 -- Create localhost user as well for local access
-CREATE USER 'jobportal'@'localhost' IDENTIFIED BY 'jobportal123';
+CREATE USER 'jobportal'@'localhost' IDENTIFIED BY 'ZpluseAppPass2025!';
 GRANT ALL PRIVILEGES ON jobportal.* TO 'jobportal'@'localhost';
 
 -- Flush privileges to ensure changes take effect
